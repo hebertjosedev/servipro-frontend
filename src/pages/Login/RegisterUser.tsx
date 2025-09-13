0c98a25fa25955fbff4dd04a9f29f672b128a711
hebertjosedev <hebertjosedev@gmail.com>
2025-09-13 12:37:57 -0300

    cambio ruta para crear usuarios

--- src/pages/Login/RegisterUser.tsx
+++ src/pages/Login/RegisterUser.tsx
@@ -52,7 +52,7 @@ const RegisterUser = () => {
           try {
             // const response = await axios.post cambiado por problema en vercel por no usar
             await axios.post(
-              `https://servipro-backend-production.up.railway.app/api/v1/auth/token`,
+              `https://servipro-backend-production.up.railway.app/api/v1/users/register`,
               {
                 full_name: name,
                 document_type: documentType,
